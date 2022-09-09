a8e2d46e668ba4488dfeb357df36a6b7931f4cb0
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-09-09 15:00:31 -0400

    creating the filter to see if the id exists to update or delete a set client

---
+++ Aula01Api/Controllers/Filters/CheckingIdExistsActionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Aula01Api.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Aula01Api.Controllers.Filters
+{
+    public class CheckingIdExistsActionFilter :ActionFilterAttribute
+    {
+        public IClientService _clientService;
+
+        public CheckingIdExistsActionFilter(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            long id = (long)context.ActionArguments["id"];
+
+            if (_clientService.GetClient(id) == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+        }
+
+    }
+}
