44c627bd98d37d835df4ebd15c6d30743d7a0ea6
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-09-09 19:01:31 -0400

    Using try catches to try catch an exception regarding the repository connection

--- Aula01Api.Infra.Data/Repository/ClientsRepository.cs
+++ Aula01Api.Infra.Data/Repository/ClientsRepository.cs
@@ -17,17 +17,33 @@ namespace Aula01Api.Infra.Data.Repository
         public List<Client> GetClients()
         {
             var query = "SELECT * FROM Clientes";
-            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            return conn.Query<Client>(query).ToList();
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                return conn.Query<Client>(query).ToList();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error Type {ex.GetType().Name}Message {ex.Message}, Stack Trace{ex.StackTrace}, Target {ex.TargetSite}");
+                return null;
+            }
+            
         }
         public Client GetClient(long id)
         {
             var query = "SELECT * FROM Clientes WHERE id = @id";
             var parameters = new DynamicParameters();
             parameters.Add("id", id);
-
-            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            return conn.QuerySingleOrDefault<Client>(query, parameters);
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                return conn.QuerySingleOrDefault<Client>(query, parameters);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine($"Error Type {ex.GetType().Name}Message {ex.Message}, Stack Trace{ex.StackTrace}, Target {ex.TargetSite}");
+                return new Client { Cpf = "0", DataNascimento = Convert.ToDateTime(0), Id = 0, Nome = ""};
+            }
         }
         public bool InsertClient(Client client)
         {
@@ -38,9 +54,17 @@ namespace Aula01Api.Infra.Data.Repository
             parameters.Add("dataNascimento", client.DataNascimento);
             parameters.Add("age", client.Age);
 
-            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            return conn.Execute(query, parameters) == 1;
-              
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                return conn.Execute(query, parameters) == 1;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error Type {ex.GetType().Name}Message {ex.Message}, Stack Trace{ex.StackTrace}, Target {ex.TargetSite}");
+                return false;
+            }
+
         }
 
         public bool UpdateClient(Client client, long id)
@@ -51,9 +75,16 @@ namespace Aula01Api.Infra.Data.Repository
             parameters.Add("cpf", client.Cpf);
             parameters.Add("nome", client.Nome);
             parameters.Add("dataNascimento", client.DataNascimento);
-
-            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            return conn.Execute(query, parameters) == 1;
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                return conn.Execute(query, parameters) == 1;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error Type {ex.GetType().Name}Message {ex.Message}, Stack Trace{ex.StackTrace}, Target {ex.TargetSite}");
+                return false;
+            }            
         }
 
         public bool DeleteClient(long id)
@@ -62,9 +93,16 @@ namespace Aula01Api.Infra.Data.Repository
             var parameters = new DynamicParameters();
             parameters.Add("id", id);
 
-            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-
-            return conn.Execute(query, parameters) == 1;
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                return conn.Execute(query, parameters) == 1;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error Type {ex.GetType().Name}Message {ex.Message}, Stack Trace{ex.StackTrace}, Target {ex.TargetSite}");
+                return false;
+            }
         }
 
         public Client GetClient(string cpf)
@@ -73,8 +111,16 @@ namespace Aula01Api.Infra.Data.Repository
             var parameters = new DynamicParameters();
             parameters.Add("cpf", cpf);
 
-            using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
-            return conn.QuerySingleOrDefault<Client>(query, parameters);
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+                return conn.QuerySingleOrDefault<Client>(query, parameters);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error Type {ex.GetType().Name}Message {ex.Message}, Stack Trace{ex.StackTrace}, Target {ex.TargetSite}");
+                return new Client { Cpf = "0", DataNascimento = Convert.ToDateTime(0), Id = 0, Nome = "" };
+            }
         }
     }
 
