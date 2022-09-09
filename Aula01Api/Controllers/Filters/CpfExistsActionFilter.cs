c0d1a9022a140228bd9db42bd9354a7367311c61
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-09-09 14:59:31 -0400

    creating the filter if the cpf exists

---
+++ Aula01Api/Controllers/Filters/CpfExistsActionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Aula01Api.Core.Interfaces;
+using Aula01Api.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Aula01Api.Controllers.Filters
+{
+    public class CpfExistsActionFilter : ActionFilterAttribute
+    {
+        public IClientService _clientService;
+        public CpfExistsActionFilter(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var client = context.ActionArguments["client"] as Client;
+
+
+            if (_clientService.GetClient(client.Cpf) != null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status409Conflict);
+            }
+        }
+    }
+}
