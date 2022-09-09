6d03eb1fe79f415ce23901001e0a14a5afd4923d
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-09-09 16:55:18 -0400

    Creating the StopWatch to calculate the elapsed time and adding it as a global filter to the aplication

---
+++ Aula01Api/Controllers/Filters/StopWatchResourceFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace Aula01Api.Controllers.Filters
+{
+    public class StopWatchResourceFilter : IResourceFilter
+    {
+        public Stopwatch sw = new();
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            sw.Stop();
+            TimeSpan ts = sw.Elapsed;
+
+            Console.WriteLine($"Elapsed time: {ts.TotalSeconds}");
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            sw.Start();
+        }
+    }
+}
