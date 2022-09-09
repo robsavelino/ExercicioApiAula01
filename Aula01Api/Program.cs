594286779fd79f0a934de97b91f7866fa5980765
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-09-09 12:46:27 -0400

    Update the Program.cs to accept the injections we are executing in the controler

--- Aula01Api/Program.cs
+++ Aula01Api/Program.cs
@@ -1,3 +1,7 @@
+using Aula01Api.Core.Interfaces;
+using Aula01Api.Core.Services;
+using Aula01Api.Infra.Data.Repository;
+
 namespace Aula01Api
 {
     public class Program
@@ -12,6 +16,9 @@ namespace Aula01Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<IClientService, ClientService>();
+            builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
             var app = builder.Build();
 
